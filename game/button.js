a4b340054c91498129c91f923138cb9e37f226cd
SwinkyDinky1 <112979344+SwinkyDinky1@users.noreply.github.com>
2022-09-06 15:00:04 -0500

    Add files via upload

---
+++ game/button.js
@@ -0,0 +1,9 @@
+// Unused
+function calibrateButtons() {
+    var buttons = document.getElementsByClassName()
+    for(i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("mousedown", () => {
+
+        })
+    }
+}
